05cd44a24c062fce2534c993678e596ae4ee61fd
Michael Gaudet <Michael.g224@gmail.com>
2015-12-11 01:40:56 -0800

    added Messages index functionality

--- app/db/migrate/20151211072611_create_messages.rb
+++ app/db/migrate/20151211072611_create_messages.rb
@@ -1,6 +1,9 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
+    	t.string :name
+    	t.string :email
+    	t.text :body
 
       t.timestamps null: false
     end
--- app/db/schema.rb
+++ app/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209100018) do
+ActiveRecord::Schema.define(version: 20151211072611) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
