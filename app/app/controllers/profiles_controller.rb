4206a136f6ee1359f865d2b47eaf3991a010b441
Michael Gaudet <Michael.g224@gmail.com>
2015-12-14 02:31:46 -0800

    Modified new action in profiles controller

--- app/app/controllers/profiles_controller.rb
+++ app/app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
 	def new 
 		# Form for users to make their profile
 		@user=User.find(params[:user_id])
-		@profile = @user.build_profile
+		@profile = Profile.new
 	end
 	
 	def create
